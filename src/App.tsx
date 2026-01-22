117d07478951b66d3c88cd37f48462a38870d3e1
prudhvi040 <prudhvimj888@gmail.com>
2026-01-22 18:07:42 +0530

    added tools

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigation } from './components/Navigation';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
+import { Tools } from './components/Tools';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 
@@ -11,7 +12,7 @@ export default function App() {
       <Navigation />
       <Hero />
       <About />
-      <Skills />
+      <Tools />
       <Projects />
       <Contact />
     </div>
